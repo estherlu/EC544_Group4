08c661f68323e5070fb6bd90b5cb5016c31583c7
Dung Pham <ptd@bu.edu>
2015-10-11 17:39:10 -0400

    socket io added

--- challenge#3/ControlServer/ControlServer.js
+++ challenge#3/ControlServer/ControlServer.js
@@ -1,5 +1,6 @@
 var SerialPort = require("serialport");
 var app = require('express')();
+var express=require('express');
 var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
@@ -21,32 +22,34 @@ app.get('/', function(req, res){
 io.on('connection', function(socket){
   console.log('luminaire connected');
   socket.on('disconnect', function(){
+    console.log('luminaire disconnected');
   });
-  socket.on('status', function(msg){
-    io.emit('status', msg);
+  socket.on('bStates', function(msg){
+    //io.emit('bStates', msg);
+    LEDStatus = msg;
     sp.write(msg + "\n");
+    console.log('message: ' + msg); // + msg
   });
 });
 
 http.listen(3001, function(){
-  console.log('listening on *:3000');
+  console.log('listening on *:3001');
 });
 
 sp.on("open", function () {
   console.log('open');
-  sp.on('data', function(data) {
-    console.log('data received: ' + data);
-    console.log('LED1 Status: ' + data[0]);
-    console.log('LED2 Status: ' + data[1]);
-    console.log('LED3 Status: ' + data[2]);
-    console.log('LED4 Status: ' + data[3]);
-    io.emit("status", "An XBee says: " + data);
+  //sp.on('data', function(data) {
+  //  console.log('data received: ' + data);
+  //  console.log('LED1 Status: ' + data[0]);
+  //  console.log('LED2 Status: ' + data[1]);
+  //  console.log('LED3 Status: ' + data[2]);
+  //  console.log('LED4 Status: ' + data[3]);
+  //  io.emit("status", "An XBee says: " + data);
     //LEDStatus = data;
-  });
+  //});
 
-  sp.write(LEDStatus, function(err, results) {
-      console.log('err ' + err);
-      console.log('results ' + results);
-  });
-
+  //sp.write(LEDStatus, function(err, results) {
+  //    console.log('err ' + err);
+  //    console.log('results ' + results);
+  //});
 });
