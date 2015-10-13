a1a28e3c2f827d4eebf498a88162fa97900827e7
Dung Pham <ptd@bu.edu>
2015-10-13 17:42:46 -0400

    open port 80 and minor changes

--- challenge#3/ControlServer/ControlServer.js
+++ challenge#3/ControlServer/ControlServer.js
@@ -32,8 +32,8 @@ io.on('connection', function(socket){
   });
 });
 
-http.listen(3001, function(){
-  console.log('listening on *:3001');
+http.listen(80, function(){
+  console.log('listening on *:80');
 });
 
 sp.on("open", function () {
