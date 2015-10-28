896ab7ad8825f1b7889bfb2615dccb9300f5aeca
lsuwayan <lsuwayan@bu.edu>
2015-10-28 14:12:17 -0400

    Added XBee Remote Control

--- challenge#4/LidarPIDwithSetpt/LidarPIDwithSetpt.ino
+++ challenge#4/LidarPIDwithSetpt/LidarPIDwithSetpt.ino
@@ -1,10 +1,13 @@
 #include <Wire.h>
+#include <SoftwareSerial.h>
 #include <PID_v1.h>
 #define    LIDARLite_ADDRESS   0x62          // Default I2C Address of LIDAR-Lite.
 #define    RegisterMeasure     0x00          // Register to write to initiate ranging.
 #define    MeasureValue        0x04          // Value to initiate ranging.
 #define    RegisterHighLowB    0x8f          // Register to get both High and Low bytes in 1 call.
 #include <Servo.h>
+SoftwareSerial XBee(2, 3); // RX, TX
+int cmnd = 49;
  
 Servo wheels; // servo for turning the wheels
 Servo esc; // not actually a servo, but controlled like one!
@@ -30,6 +33,7 @@ void setup() {
   Serial.begin(9600); //Opens serial connection at 9600bps.
   wheels.attach(8); // initialize wheel servo to Digital IO Pin #8
   esc.attach(9); // initialize ESC to Digital IO Pin #9
+  XBee.begin(9600);
   /*  If you're re-uploading code via USB while leaving the ESC powered on, 
    *  you don't need to re-calibrate each time, and you can comment this part out.
    */
@@ -88,60 +92,86 @@ void loop() {
   // read the analog output of the EZ1 from analog input 0
   sensor_1 = analogRead(0);
   sensor_2 = analogRead(2);
+
   
   // convert the sensor reading to inches
   inches_1 = sensor_1 / 2; //512 = 254 inches,
   inches_2 = sensor_2 / 2; //512 = 254 inches,
+
+
+  if (XBee.available())
+  {
+      int tmp = XBee.read();
+     if (tmp == 48 || tmp == 49)
+     {
+        cmnd = tmp;
+        Serial.print("xbee");
+        Serial.println(cmnd);
+     }
+     if(cmnd==49) //start
+     {
+       esc.write(75);
+       Serial.println("start");
+     }
+     if(cmnd==48)
+     {
+        esc.write(90);
+       Serial.println("stop");
+     }
+  }
   
-  if ((inches_1 <= 15 and inchesLast_1 <= 15) || (inches_2 <= 15 and inchesLast_2 <= 15)) esc.write(90);
-  else esc.write(75);
-  inchesLast_1 = inches_1;
-  inchesLast_2 = inches_2;
+  if (cmnd == 49)
+  {
+    if((inches_1 <= 15 and inchesLast_1 <= 15) || (inches_2 <= 15 and inchesLast_2 <= 15)) esc.write(90);
+    else esc.write(75);
+    inchesLast_1 = inches_1;
+    inchesLast_2 = inches_2;
+    
+    // print out the decimal result
+    Serial.print("EZ1: ");
+    Serial.println(inches_1,DEC);
+    Serial.println(inches_2,DEC);
   
-  // print out the decimal result
-  Serial.print("EZ1: ");
-  Serial.println(inches_1,DEC);
-  Serial.println(inches_2,DEC);
+    Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+    Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)
+    Wire.write((int)MeasureValue); // sets register pointer to  (0x00)
+    Wire.endTransmission(); // stop transmitting
+    delay(20); // Wait 20ms for transmit
 
-      Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-      Wire.write((int)RegisterMeasure); // sets register pointer to  (0x00)
-      Wire.write((int)MeasureValue); // sets register pointer to  (0x00)
-      Wire.endTransmission(); // stop transmitting
-      delay(20); // Wait 20ms for transmit
+    Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
+    Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
+    Wire.endTransmission(); // stop transmitting
 
-      Wire.beginTransmission((int)LIDARLite_ADDRESS); // transmit to LIDAR-Lite
-      Wire.write((int)RegisterHighLowB); // sets register pointer to (0x8f)
-      Wire.endTransmission(); // stop transmitting
+    delay(20); // Wait 20ms for transmit
 
-      delay(20); // Wait 20ms for transmit
+    Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
 
-      Wire.requestFrom((int)LIDARLite_ADDRESS, 2); // request 2 bytes from LIDAR-Lite
-
-      if (2 <= Wire.available()) // if two bytes were received
-      {
-        dist = Wire.read(); // receive high byte (overwrites previous reading)
-        dist = dist << 8; // shift high byte to be high 8 bits
-        dist |= Wire.read(); // receive low byte as lower 8 bits
-      }
-      
-          if (dist < 0 || dist > 130) {
-            dist = l_0;
-          }
-          dist = 0.7 * dist + 0.3 * l_0;
-          l_0 = dist;
-          
-    Input = dist;
-
-    // use PID loop
-    PIDleft.Compute();
-    wheels.write(110 + Output);
-
-    // Print Distance
+    if (2 <= Wire.available()) // if two bytes were received
+    {
+      dist = Wire.read(); // receive high byte (overwrites previous reading)
+      dist = dist << 8; // shift high byte to be high 8 bits
+      dist |= Wire.read(); // receive low byte as lower 8 bits
+    }
     
-        //Serial.print("Dist0: ");
-        //Serial.print(dist);
-        //Serial.print(", PID0: ");
-        //Serial.println(Output);
+    if (dist < 0 || dist > 130) {
+      dist = l_0;
+    }
+    dist = 0.7 * dist + 0.3 * l_0;
+    l_0 = dist;
     
-    delay(100);
+  Input = dist;
+
+  // use PID loop
+  PIDleft.Compute();
+  wheels.write(110 + Output);
+
+  // Print Distance
+  
+      //Serial.print("Dist0: ");
+      //Serial.print(dist);
+      //Serial.print(", PID0: ");
+      //Serial.println(Output);
+  
+  delay(100);
+  }
   }
