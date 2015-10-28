392feace5c66f80083afc6c138d2432f3cc8e85b
lsuwayan <lsuwayan@bu.edu>
2015-10-28 19:53:38 -0400

    Speed XBee Chat

---
+++ challenge#4/Speed XBee/xbeeChat_Speed.js
@@ -0,0 +1,42 @@
+var SerialPort = require("serialport");
+var app = require('express')();
+var http = require('http').Server(app);
+var io = require('socket.io')(http);
+
+var portName = process.argv[2],
+portConfig = {
+	baudRate: 9600,
+	parser: SerialPort.parsers.readline("\n")
+};
+
+var sp;
+
+sp = new SerialPort.SerialPort(portName, portConfig);
+
+app.get('/', function(req, res){
+  res.sendfile('speed.html');
+});
+
+io.on('connection', function(socket){
+  console.log('a user connected');
+  socket.on('disconnect', function(){
+  });
+  socket.on('chat message', function(msg){
+    io.emit('chat message', msg);
+    sp.write(msg + "\n");
+  });
+});
+
+http.listen(5000, function(){
+  console.log('listening on *:5000');
+});
+
+sp.on("open", function () {
+  console.log('open');
+  sp.on('data', function(data) {
+    console.log('data received: ' + data);
+    var wheelSpeed = (55.9/24.0) * (1000.0/data);
+    io.emit("chat message", "Speed is: " + wheelSpeed);
+  });
+});
+
