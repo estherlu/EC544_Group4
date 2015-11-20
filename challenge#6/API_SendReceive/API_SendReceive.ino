5a0db5e05dcaf81bf59efa77fe7a8e9c6202b320
lsuwayan <lsuwayan@bu.edu>
2015-11-20 17:41:04 -0500

    Updated Code

--- challenge#6/API_SendReceive/API_SendReceive.ino
+++ challenge#6/API_SendReceive/API_SendReceive.ino
@@ -1,5 +1,3 @@
-
- 
 #include <XBee.h>
 #include <SoftwareSerial.h>
 
@@ -26,12 +24,15 @@ AtCommandResponse atResponse = AtCommandResponse();
 SoftwareSerial nss(ssRX, ssTX);
 SoftwareSerial xbeeSerial(2,3);
 
-
-uint8_t id = '1';
-int id_list[4];
-unsigned long id_time[4];
+int red=7;
+int blue=6;
+int green=5;
+int switchState = 0;
+uint8_t id = '3';
+int id_list[5];
+unsigned long id_time[5];
 int leaderID = 0;
-boolean leader = false;
+bool leader = true;
 
 void setup() {  
   // start serial
@@ -39,12 +40,18 @@ void setup() {
   xbeeSerial.begin(9600);
   xbee.setSerial(xbeeSerial);
   Serial.println("Initializing...");
+  pinMode(green, OUTPUT);
+  pinMode(blue, OUTPUT);
+  pinMode(red, OUTPUT);
+
+  pinMode(8, INPUT);
+  digitalWrite(green,HIGH);
 }
 
 void loop() 
 {
       /*Send Message*/
-      uint8_t payload[] = {'X', 'X', '4', '4'};
+      uint8_t payload[] = {'X', 'X', id};
       // Specify the address of the remote XBee (this is the SH + SL)
       XBeeAddress64 addr64 = XBeeAddress64(0x00000000, 0x0000FFFF);
       // Create a TX Request
@@ -67,43 +74,67 @@ void loop()
               uint8_t data = rx.getData()[j];
               if (data == '1')
               {
-                id_list[0] = 1;
-                id_time[0] = millis();
+                delay(100);
+                id_list[1] = 1;
+                id_time[1] = millis();
                 //Serial.write(rx.getData()[j]);
                 Serial.println("XXXXXXXX  1 XXXXXXXXX");
-                Serial.write(id_list[0]);
-                Serial.write(id_time[0]);
+                Serial.write(id_list[1]);
+                Serial.write(id_time[1]);
+                //Serial.write(millis());
               }
               else if (data == '2')
               {
-                id_list[1] = 2;
-                id_time[1] = millis();
+                id_list[2] = 2;
+                id_time[2] = millis();
                 //Serial.write(rx.getData()[j]);
                 Serial.println("XXXXXXXX  2 XXXXXXXXX");
-                Serial.print(id_list[1]);
-                Serial.print(id_time[1]);
-              }
-              else if (data == '3')
-              {
-                id_list[2] = 3;
-                id_time[2] = millis();
-                Serial.println("XXXXXXXX  3 XXXXXXXXX");
                 Serial.print(id_list[2]);
                 Serial.print(id_time[2]);
               }
-              else if (data == '4')
+              else if (data == '3')
               {
-                id_list[3] = 4;
+                id_list[3] = 3;
                 id_time[3] = millis();
-                Serial.println("XXXXXXXX  4 XXXXXXXXX");
+                Serial.println("XXXXXXXX  3 XXXXXXXXX");
                 Serial.print(id_list[3]);
                 Serial.print(id_time[3]);
+              }
+              else if (data == '4')
+              {
+                id_list[4] = 4;
+                id_time[4] = millis();
+                Serial.println("XXXXXXXX  4 XXXXXXXXX");
+                Serial.print(id_list[4]);
+                Serial.print(id_time[4]);
               }
            }
            break;
         }
       }
       Serial.println();
- 
+
+      if (((id_time[1] + 1000) > millis()) || ((id_time[2] + 1000) > millis())) {
+        leader = false;
+      }
+      else leader = true;
+      
+  switchState=digitalRead(8);
+  //(write code)if the ID is lowest Leader == 1
+
+  if(leader)
+  {
+    digitalWrite(blue,HIGH);
+    digitalWrite(green,LOW);
+    digitalWrite(red,LOW);
+  }
+  else
+  { 
+    if(switchState==1)
+    { digitalWrite(red, HIGH);
+      digitalWrite(green,LOW);
+    }
+  
+  }
     delay(300);
 }
